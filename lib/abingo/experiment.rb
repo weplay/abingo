7d036a4c48c0685454cf3ed04920111f61dd0e69
Lee Bankewitz <leebankewitz@duderino.local>
2009-11-18 17:10:10 -0500

    explicitly name class names with module scope to avoid "object is not missing constant" errors when referencing Abingo classes  in rails initializers

--- lib/abingo/experiment.rb
+++ lib/abingo/experiment.rb
@@ -2,7 +2,7 @@ class Abingo::Experiment < ActiveRecord::Base
   include Abingo::Statistics
   include Abingo::ConversionRate
 
-  has_many :alternatives, :dependent => :destroy
+  has_many :alternatives, :class_name => "Abingo::Alternative", :dependent => :destroy
   validates_uniqueness_of :test_name
 
   def before_destroy
