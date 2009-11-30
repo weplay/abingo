a4bdb860e306ae40c12f999d96a2c9879e3d036c
Lee Bankewitz <leebankewitz@duderino.local>
2009-11-30 11:49:40 -0500

    Use top to alternatives to calc zscore for experiments with more than 2 alternatives.

--- lib/abingo/statistics.rb
+++ lib/abingo/statistics.rb
@@ -4,31 +4,35 @@
 module Abingo::Statistics
 
   HANDY_Z_SCORE_CHEATSHEET = [[0.10, 1.29], [0.05, 1.65], [0.01, 2.33], [0.001, 3.08]]
-  
+
   PERCENTAGES = {0.10 => '90%', 0.05 => '95%', 0.01 => '99%', 0.001 => '99.9%'}
-  
+
   DESCRIPTION_IN_WORDS = {0.10 => 'fairly confident', 0.05 => 'confident',
                          0.01 => 'very confident', 0.001 => 'extremely confident'}
   def zscore
-    if alternatives.size != 2
-      raise "Sorry, can't currently automatically calculate statistics for A/B tests with > 2 alternatives."
-    end
+    alternatives_for_zscore = top_2_alternatives
 
-    if (alternatives[0].participants == 0) || (alternatives[1].participants == 0)
+    if (alternatives_for_zscore[0].participants == 0) || (alternatives_for_zscore[1].participants == 0)
       raise "Can't calculate the z score if either of the alternatives lacks participants."
     end
 
-    cr1 = alternatives[0].conversion_rate
-    cr2 = alternatives[1].conversion_rate
+    cr1 = alternatives_for_zscore[0].conversion_rate
+    cr2 = alternatives_for_zscore[1].conversion_rate
 
-    n1 = alternatives[0].participants
-    n2 = alternatives[1].participants
+    n1 = alternatives_for_zscore[0].participants
+    n2 = alternatives_for_zscore[1].participants
 
     numerator = cr1 - cr2
     frac1 = cr1 * (1 - cr1) / n1
     frac2 = cr2 * (1 - cr1) / n2
 
     numerator / ((frac1 + frac2) ** 0.5)
+  end
+
+  def top_2_alternatives
+    alternatives_with_participants = alternatives.reject {|a| a.conversion_rate.nan? }
+    sorted_alternatives = alternatives_with_participants.sort {|a,b| b.conversion_rate <=> a.conversion_rate }
+    sorted_alternatives.slice(0,2)
   end
 
   def p_value
@@ -48,12 +52,13 @@ module Abingo::Statistics
   def is_statistically_significant?(p = 0.05)
     p_value <= p
   end
-  
+
   def pretty_conversion_rate
     sprintf("%4.2f%%", conversion_rate * 100)
   end
 
   def describe_result_in_words
+    alternatives_to_describe = alternatives
     begin
       z = zscore
     rescue
@@ -61,19 +66,19 @@ module Abingo::Statistics
     end
     p = p_value
 
+    best  = alternatives_to_describe[0]
+    worst = alternatives_to_describe[1]
+
     words = ""
-    if (alternatives[0].participants < 10) || (alternatives[1].participants < 10)
+    if (best.participants < 10) || (worst.participants < 10)
       words += "Take these results with a grain of salt since your samples are so small: "
     end
 
-    alts = alternatives - [best_alternative]
-    worst_alternative = alts.first
-
-    words += "The best alternative you have is: [#{best_alternative.content}], which had "
-    words += "#{best_alternative.conversions} conversions from #{best_alternative.participants} participants "
-    words += "(#{best_alternative.pretty_conversion_rate}).  The other alternative was [#{worst_alternative.content}], "
-    words += "which had #{worst_alternative.conversions} conversions from #{worst_alternative.participants} participants "
-    words += "(#{worst_alternative.pretty_conversion_rate}).  "
+    words += "The best alternative you have is: [#{best.content}], which had "
+    words += "#{best.conversions} conversions from #{best.participants} participants "
+    words += "(#{best.pretty_conversion_rate}).  The other alternative was [#{worst.content}], "
+    words += "which had #{worst.conversions} conversions from #{worst.participants} participants "
+    words += "(#{worst.pretty_conversion_rate}).  "
 
     if (p.nil?)
       words += "However, this difference is not statistically significant."
